0c2cd4350dfd019ece1d02b7b0ce31170b4fa904
Anfalelterefi <anfal123musa@gmail.com>
2023-03-27 07:59:28 -0700

    0x05 exceptions

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def raise_exception():
+
+
+    """Raise a TypeError exception."""
+    raise TypeError
+
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+    Terms
+    Privacy
